a6b9c92abac1a55eda8e876d18e4910aad62dde4
TatuMon <ezurkanmn@gmail.com>
2021-06-02 21:04:25 -0300

    Created Community migrations and updated posts migrations

--- database/migrations/2021_06_02_235043_create_communities_table.php
+++ database/migrations/2021_06_02_235043_create_communities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCommunitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('communities', function (Blueprint $table) {
             $table->id();
-            $table->string('slug')->unique();
-            $table->string('title');
-            $table->foreignId('community_id');
-            $table->text('excerpt');
-            $table->text('body');
-            $table->timestamp('published_at')->nullable();
+            $table->string('name')->unique();
+            $table->integer('members');
+            $table->string('created_by');
+            $table->timestamp('comm_created_at');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('communities');
     }
 }
