ec80dda35a863c8af7626b24e6a7c4463f0fd45d
TatuMon <ezurkanmn@gmail.com>
2021-06-04 15:52:55 -0300

    Minor fix in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Community;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -33,6 +34,30 @@ class DatabaseSeeder extends Seeder
             'name' => 'Dark Souls',
             'slug' => 'dark-souls',
             'description' => 'We love to get beaten again and again'
+        ]);
+
+        Post::create([
+            'title' => 'Should u play any Metro game?',
+            'slug' => 'should-u-play-any-metro-game',
+            'community_id' => 1,
+            'excerpt' => 'Yes.',
+            'body' => 'Yes.'
+        ]);
+
+        Post::create([
+            'title' => 'Should u play any Dishonored game?',
+            'slug' => 'should-u-play-any-dishonored-game',
+            'community_id' => 2,
+            'excerpt' => 'Yes.',
+            'body' => 'Yes.'
+        ]);
+
+        Post::create([
+            'title' => 'Should u play any Dark Souls game?',
+            'slug' => 'should-u-play-any-dark-souls-game',
+            'community_id' => 3,
+            'excerpt' => 'If u want to suffer, yes.',
+            'body' => 'If u want to suffer, yes.'
         ]);
     }
 }
