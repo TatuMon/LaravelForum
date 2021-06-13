b58838463211b4a67bc12e6d5d8f6a3626ef8425
TatuMon <ezurkanmn@gmail.com>
2021-06-13 20:25:54 -0300

    name changed from post-excerpt

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">Good Mambo: the front page of the Branca Team</x-slot>
     @if (count($posts))
         @foreach($posts as $post)
-            <x-post-excerpt :post="$post" />
+            <x-post-card :post="$post" />
         @endforeach
     @else
         <h1 class="no_posts_alert">No posts were found :(</h1>
