6d27d4f44602c5cd7efa6eccdf573335b913a7ec
TatuMon <ezurkanmn@gmail.com>
2021-06-17 16:46:25 -0300

    Successfuly improved :)

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Community;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommunityController extends Controller
@@ -10,19 +11,7 @@ class CommunityController extends Controller
     public function search(Community $community){
         return view('community', [
             'name' => $community->name,
-            'posts' => $community->posts->filter(function($post){
-                if(request('search')){
-                    if(str_contains(strtolower($post->title), strtolower(request('search')))){
-                        return true;
-                    } elseif(str_contains(strtolower($post->body), strtolower(request('search')))) {
-                        return true;
-                    }  else {
-                        return false;
-                    }
-                } else {
-                    return true;
-                }
-            })->load(['community', 'user']),
+            'posts' => Post::whereHas('community', fn($query) => $query->where('name', '=', $community->name))->with('community')->filter(request(['search']))->get(),
             'comms' => Community::select('name', 'slug')->get()
         ]);
     }
--- app/Models/Community.php
+++ app/Models/Community.php
@@ -10,8 +10,7 @@ class Community extends Model
     use HasFactory;
 
     public function scopeFilter($query, array $filters){
-        $query->when($filters['search_posts'] ?? false, fn() => $query->posts->where('title', 'like', '%' . request('search') . '%')
-        ->orWhere('body', 'like', '%' . request('search') . '%'));
+        
     }
 
     public function posts(){
