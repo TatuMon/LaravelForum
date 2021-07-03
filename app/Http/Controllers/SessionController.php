908f52eb8bd3a4fb35870c65c52c129b5bb1a97c
TatuMon <ezurkanmn@gmail.com>
2021-07-02 21:14:27 -0300

    Created session controller

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
-    //
+    public function destroy(){
+        Auth::logout();
+    }
 }
