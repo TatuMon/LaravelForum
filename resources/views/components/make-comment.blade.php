02beaca729bd7b270983263ad90b80b1c78e364b
TatuMon <ezurkanmn@gmail.com>
2021-07-12 19:23:00 -0300

    Yet another style improvement

--- resources/views/components/make-comment.blade.php
+++ resources/views/components/make-comment.blade.php
@@ -1,6 +1,6 @@
 <div class="comment-creator-box">
     @auth
-        <span class="comment-as">Comment as {{ auth()->user->username }}</span>
+        <span class="comment-as">Comment as <span style="color: #33a8ff">{{ auth()->user()->username }}</span></span>
     @else
         <span class="comment-as">Login to comment</span>
     @endauth
