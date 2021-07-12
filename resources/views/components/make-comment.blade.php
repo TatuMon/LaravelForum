a032b919fd0d8bb7fa38ae14267a37e1186d6330
TatuMon <ezurkanmn@gmail.com>
2021-07-12 18:11:32 -0300

    Now yes style comment

--- resources/views/components/make-comment.blade.php
+++ resources/views/components/make-comment.blade.php
@@ -1,10 +1,10 @@
-<div>
-    <div>
-        <p>Comment as <span>(aca iria el usuario que esta logueado)</span></p>
-    </div>
-    <div>
-        <textarea>
-
-        </textarea>
-    </div>
+<div class="comment-creator-box">
+    @auth
+        <span class="comment-as">Comment as {{ auth()->user->username }}</span>
+    @else
+        <span class="comment-as">Login to comment</span>
+    @endauth
+    
+    <textarea class="comment-creator">
+    </textarea>
 </div>
