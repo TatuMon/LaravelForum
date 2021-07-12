c8ff35d90ea40776968bb269bc90de819ad6a236
TatuMon <ezurkanmn@gmail.com>
2021-07-12 17:48:13 -0300

    Style comment

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,10 +1,12 @@
 @props(['comment'])
 
-<section>
+<section class="comment-box">
     <div>
+        <span><img src="https://i.pravatar.cc/45" alt="" class="user-logo"></span>
         <span><a class="user_name" href="/usr/{{ $comment->user->slug }}">{{ $comment->user->username }}</a></span>
         <span>•</span>
         <span>{{ $comment->created_at->diffForHumans() }}</span>
     </div>
     <p>{{ $comment->content }}</p>
+    <a href="/respond"><button class="respond">respond</button></a>
 </section>
