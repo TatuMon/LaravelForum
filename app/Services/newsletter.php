739b631fcf2ad91141979cfe26916624c62600a9
TatuMon <ezurkanmn@gmail.com>
2021-07-18 18:01:58 -0300

    Beautify Newsletter class

--- app/Services/newsletter.php
+++ app/Services/newsletter.php
@@ -6,22 +6,27 @@ use Exception;
 use MailchimpMarketing\ApiClient;
 
 class Newsletter{
-    public function subscribe(){
+    protected $client;
+
+    function __construct()
+    {
         $client = new ApiClient();
         $client->setConfig([
             'apiKey' => config('services.mailchimp.key'),
             'server' => 'us6',
         ]);
+    }
 
+    public function subscribe(){
         try{
-            $response = $client->lists->addListMember('f61a87042a', [
+            $response = $this->client->lists->addListMember(config('services.mailchimp.lists.subscribers'), [
                 'email_address' => auth()->user()->email,
                 'status' => 'subscribed'
             ]);
 
             return back()->with('success', "We'll keep you updated from now on");
         } catch(Exception $e) {
-            back()->with('error', $e->getMessage());
+            return back()->with('error', "Your email is invalid or already signed in");
         }
     }
 }
--- config/services.php
+++ config/services.php
@@ -31,6 +31,9 @@ return [
     ],
 
     'mailchimp' => [
-        'key' => env('MAILCHIMP_KEY')
+        'key' => env('MAILCHIMP_KEY'),
+        'lists' => [
+            'subscribers' => 'f61a87042a'
+        ]
     ]
 ];
