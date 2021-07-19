010d8f336a28bdb523bdf1cfc2aaadaf704e9e2d
TatuMon <ezurkanmn@gmail.com>
2021-07-19 14:50:37 -0300

    Add a little better style to user profile

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -2,7 +2,7 @@
 
 <section class="comment-box">
     <div class="info">
-        <span><img src="https://i.pravatar.cc/45" alt="" class="user-logo"></span>
+        <span><img src="https://i.pravatar.cc/100" alt="" class="user-logo"></span>
         <span><a class="user_name" href="/usr/{{ $comment->user->slug }}">{{ $comment->user->username }}</a></span>
         <span>•</span>
         <span>{{ $comment->created_at->diffForHumans() }}</span>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,9 +1,12 @@
 <x-layout :comms="$comms">
     <x-slot name="title">{{ $user->username }}</x-slot>
-
-    <h1> {{ $user->username }} </h1>    
-
+    <div id="main-info">
+        <img src="https://i.pravatar.cc/50" alt="" id="profile-pic">
+        <div>
+            <h1 class="prof-usr"> usr/{{ $user->username }} </h1>
+            <span class="prof-role">{{ $user->role }}</span>
+        </div>     
+    </div>   
     <hr>
-
     <x-posts-list :posts="$posts" />
 </x-layout>
