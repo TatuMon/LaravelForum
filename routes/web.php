a51a41afa0cb49b07c98a526b9ad631f27aa1035
TatuMon <ezurkanmn@gmail.com>
2021-07-23 12:39:34 -0300

    Add middleware to admin actions

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'search'])->name('home');
 
 Route::get('/post/{post:slug}', [PostController::class, 'found'])->name('post');
-Route::get('/create-post', [PostController::class, 'creator']);
+Route::get('/create-post', [PostController::class, 'creator'])->middleware('auth');
 Route::post('/delete', [PostController::class, 'delete']);
 Route::get('/comm/{community:slug}', [CommunityController::class, 'search'])->name('comm');
 
-Route::get('/newsletter', NewsletterController::class);
+Route::get('/newsletter', NewsletterController::class)->middleware('auth');
 
 Route::get('/usr/{user:username}', [UserController::class, 'found'])->name('usr');
 Route::get('/change-pic', [UserController::class, 'changer']);
 Route::post('/changePic', [UserController::class, 'changePic']);
-Route::post('/giveRol', [UserController::class, 'giveRol']);
-Route::post('/ban', [UserController::class, 'ban']);
-Route::post('/unban', [UserController::class, 'unban']);
+Route::post('/giveRol', [UserController::class, 'giveRol'])->middleware('admin');
+Route::post('/ban', [UserController::class, 'ban'])->middleware('admin');
+Route::post('/unban', [UserController::class, 'unban'])->middleware('admin');
 
 Route::get('/register', [RegisterController::class, 'register'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'submitData'])->middleware('guest');
@@ -45,4 +45,4 @@ Route::get('/login', [SessionController::class, 'index'])->middleware('guest');
 Route::post('/login', [SessionController::class, 'login'])->middleware('guest');
 Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth');
 
-Route::post('post/{post:slug}/comment', [CommentController::class, 'store']);
+Route::post('post/{post:slug}/comment', [CommentController::class, 'store'])->middleware('auth');
