e4b1178e7ecb158b69b6895c10685e54ee0ac880
TatuMon <ezurkanmn@gmail.com>
2021-08-22 01:51:23 -0300

    Post edit btn style

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,22 +4,25 @@
     <div id="up-container">
         <article>
             <div class="post-info" x-data="{show : false}">
-                <span><a class="community_name" href="/comm/{{ $post->community->slug }}">comm/{{ $post->community->name }}</a></span>
+                <span><a class="community_name underline" href="/comm/{{ $post->community->slug }}">comm/{{ $post->community->name }}</a></span>
                 <span>•</span>
-                <span>posted by <a class="user_name" href="/usr/{{ $post->user->slug }}">{{ $post->user->username }}</a></span>
+                <span>posted by <a class="user_name underline" href="/usr/{{ $post->user->slug }}">{{ $post->user->username }}</a></span>
                 <span>{{ $post->created_at->diffForHumans() }}</span>
                 @auth
                     @if(auth()->user()->role == 'Black Hand of Branca' || $post->user->id == auth()->user()->id)
-                        <span class="danger" @click="show = !show">Delete</span>
-                        <form method="POST" action="/delete" x-show="show" @click.away="show = !show" id="delete-post">
-                            @csrf
-                            <span style="position: absolute; right: 10px; top: 0px; cursor: pointer" @click="show = !show">X</span>
-                            <h1>Sure you want to delete this post?</h1>
-                            <p><i>"{{ $post->title }}"</i></p>
-                            <p>Once deleted, you can't get it back</p>
-                            <input type="text" name="post" value="{{ $post->id }}" x-show="false"/>
-                            <input type="submit" value="Delete" class="danger"/>
-                        </form>
+                        <div id="post-opts">
+                            <span class="danger" @click="show = !show">Delete</span>
+                            {{-- <form method="POST" action="/delete" x-show="show" @click.away="show = !show" id="delete-post">
+                                    @csrf
+                                    <span style="position: absolute; right: 10px; top: 0px; cursor: pointer" @click="show = !show">X</span>
+                                    <h1>Sure you want to delete this post?</h1>
+                                    <p><i>"{{ $post->title }}"</i></p>
+                                    <p>Once deleted, you can't get it back</p>
+                                    <input type="text" name="post" value="{{ $post->id }}" x-show="false"/>
+                                    <input type="submit" value="Delete" class="danger"/>
+                                </form> --}}
+                            <span class="edit-btn"><a href="editor" >Edit</a></span>
+                        </div>
                     @endif
                 @endauth
             </div>
