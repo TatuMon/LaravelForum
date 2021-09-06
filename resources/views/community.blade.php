b2176fefc7737a13ce8ddefc1923e18226178879
TatuMon <ezurkanmn@gmail.com>
2021-09-06 16:55:14 -0300

    Capitalize comm name

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -1,6 +1,6 @@
 <x-layout :actualComm="$actualComm">
     <x-slot name="title"> {{ $name }} </x-slot>
-    <h1 style="text-align: center"> {{ $name }} </h1>    
+    <h1 style="text-align: center"> {{ ucfirst($name) }} </h1>    
     <x-comm-info :comm="$actualComm"/>
     <hr>
 
