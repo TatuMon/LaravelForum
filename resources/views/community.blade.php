b4e4c75b21ebf4e589ec61030b08745ce653c3df
TatuMon <ezurkanmn@gmail.com>
2021-09-06 16:48:05 -0300

    Comm info change

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -1,13 +1,12 @@
 <x-layout :actualComm="$actualComm">
     <x-slot name="title"> {{ $name }} </x-slot>
     <h1 style="text-align: center"> {{ $name }} </h1>    
-
+    <x-comm-info :comm="$actualComm"/>
     <hr>
 
     <div id="up-container">
         <div id="comm-posts">
             <x-posts-list :posts="$posts"/>
         </div>
-        <x-comm-info :comm="$actualComm"/>
     </div>
 </x-layout>
--- resources/views/components/comm-info.blade.php
+++ resources/views/components/comm-info.blade.php
@@ -1,9 +1,7 @@
 @props(['comm'])
 
 <div id="comm-info">
-    <h3 id="comm-name">{{ $comm->name }}</h3>
     <p id="comm-desc">{{ $comm->description }}</p>
-    <hr>
     <p id="comm-date">Created on {{ $comm->created_at->format('d-m-Y') }}</p>
     <p id="comm-posts-amount">Total posts: {{ $comm->posts->count() }}</p>
 </div>
