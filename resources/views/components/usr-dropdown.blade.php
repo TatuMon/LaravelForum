65aecd0dc33c9d1bda5949aa63810f217fe9cc28
TatuMon <ezurkanmn@gmail.com>
2021-09-06 12:45:35 -0300

    Added profile link to user dropdown menu

--- resources/views/components/usr-dropdown.blade.php
+++ resources/views/components/usr-dropdown.blade.php
@@ -1,6 +1,7 @@
 <div id="usr-dropdown" x-data="{show : false}" @click.away="show = false">
     <div @click="show = !show" class="username">{{ auth()->user()->username }} <i class="fas fa-caret-down"></i></div>
     <div x-show="show" class="usr-options dropdown">
+        <div><i class="fas fa-user"></i> <a href="/usr/{{ auth()->user()->username }}">Profile</a></div>
         <div><i class="fas fa-image"></i> <a href="/change-pic">Change profile pic</a></div>
         <form method="POST" action="/logout" id="logout">
             @csrf
